14a2b322ca1ed804cad036eaebdee0acc376682e
cgasper79 <51058790+cgasper79@users.noreply.github.com>
2020-12-21 15:16:23 +0100

    change version in footer

--- web/footer.php
+++ web/footer.php
@@ -1,6 +1,6 @@
 
 <div class="text-center text-white">
-    Versión 0.1.0 |
+    Versión 1.0.0 |
     <a class="text-white" href= 'https://github.com/cgasper79/Mask-Control'>Visita Github</a>   
 </div>
 
