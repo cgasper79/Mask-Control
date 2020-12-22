8737b1a3993e6c19313e0dc10f9a45378f5e170e
cgasper79 <51058790+cgasper79@users.noreply.github.com>
2020-12-22 08:44:06 +0100

    update

---
+++ web/edicion_OLD.php
@@ -0,0 +1,60 @@
+<?php 
+    include ('conexion.php');
+    $email = $_SESSION [ "email" ];
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM mascarilla WHERE id_mask = $id ";  
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+        require_once ('header.php');
+    ?>  
+</head>
+<body>
+
+    <?php
+        require_once ('menu.php');
+    ?>  
+
+    <div class="container-fluid">
+        <h2>Edición mascarilla</h2>
+        <?php 
+		    $result=mysqli_query($conexion,$sql);
+	        while($mostrar=mysqli_fetch_array($result)){  
+        ?>
+        <div class='form-group'>   
+            <form action = "actualizar.php" method = "POST">
+            <label>Descripción</label>
+            <p><input type="text" value = "<?php echo $mostrar['description'] ?>" name = "descripcion"></p>
+            <label>Fecha primer uso</label>
+            <p><input type="text" 
+                   value = "<?php $date = date_create($mostrar['date_ini']);
+                                  echo date_format($date,'d-m-Y'); ?>" 
+                                    name = "fecha">
+            </p>
+            <label class='ficha'>Lavados Restantes</label>
+            <p><input type="text" value = "<?php echo $mostrar['wash_max'] ?>" name = "lavados"></p>
+            <input type="hidden" value = "<?php echo $mostrar['id_mask'] ?>" name = "id">
+            <button type='submit' Onclick='return ConfirmUpdate()' class='btn btn-primary btn-block btn-lg' value='Actualizar'> Actualizar </button>
+            <a href='index.php'> <button type='button' class="btn btn-light btn-block btn-lg"> Cancelar </button></a>
+            <a href='eliminar.php?id=<?php echo $mostrar['id_mask'];?>'> <button type='button' Onclick='return ConfirmDelete()' class="btn btn-danger btn-block btn-lg">Eliminar</button></a>
+        </div>       
+        <?php 
+            } mysqli_free_result($result);
+	    ?>        
+        </form>       
+    </div>
+</body>
+
+<div class="container-fluid p-3 my-3 bg-primary text-white">
+    <footer>
+        <?php
+            require_once ('footer.php');
+        ?>  
+    </footer>
+</div>
+
+</html>
