3e30d48e8dd68f5208ea87e02c1f0971a5f17058
Cristian <51058790+cgasper79@users.noreply.github.com>
2021-01-15 14:47:55 +0100

    Update conexion.php

--- web/includes/conexion.php
+++ web/includes/conexion.php
@@ -3,7 +3,7 @@
 //leemos los datos de la base de datos configurada en VAR de Heroku
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
-username = $url["user"];
+$username = $url["user"];
 $database = substr($url["path"], 1);
 $password = $url["pass"];
 
