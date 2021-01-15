aa7fba5ce94a668f3f56bce28ed603fd561cbfa5
Cristian <51058790+cgasper79@users.noreply.github.com>
2021-01-15 12:28:54 +0100

    Update conexion.php

--- web/includes/conexion.php
+++ web/includes/conexion.php
@@ -1,18 +1,18 @@
 <?php 
 
 //leemos los datos de la base de datos configurada en VAR de Heroku
-/*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
 username = $url["user"];
 $database = substr($url["path"], 1);
 $password = $url["pass"];
-*/
 
-$servername = 'localhost';
+
+/*$servername = 'localhost';
 $username = 'cristian';
 $database = 'mask_db';
 $password = 'gascoing1979';
-
+*/
 session_start();
 //definimos tiempo de sesión en 1 hora
 define( 'MAX_SESSION_TIEMPO', 3600 * 1 );
