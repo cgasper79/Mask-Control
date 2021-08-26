8f1f4db88d26123f1acebd2b2441e8526503f50f
Cristian <51058790+cgasper79@users.noreply.github.com>
2021-08-26 07:43:14 +0200

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,17 +1,11 @@
 <?php 
 
 //leemos los datos de la base de datos configurada en VAR de Heroku
-/*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
 $username = $url["user"];
 $database = substr($url["path"], 1);
 $password = $url["pass"];
-*/
-
-$servername = 'localhost';
-$username = 'cristian';
-$database = 'mask_db';
-$password = 'gascoing1979';
 
 
 ?>
